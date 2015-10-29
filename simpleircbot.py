ae1ff2740cb7ca688070dc301ac5fbe8c7ea79f5
jgeigerm <jmg2967@rit.edu>
2015-10-29 13:35:43 -0400

    mostly done, added recv loop

--- simpleircbot.py
+++ simpleircbot.py
@@ -1,12 +1,26 @@
 import socket
+from threading import Thread
+from functools import wraps
+from time import sleep
+
+def thread(func):
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        func_hl = Thread(target = func, args = args, kwargs = kwargs)
+        func_hl.start()
+        return func_hl
+    return async_func
 
 class IRCBot(object):
 
-    def __init__(self, server_tuple, nick, channel_list):
-        self.server_tuple =  server_tuple
+    def __init__(self, server_tuple, nick, channel_list, debug=True, quiet=False):
+        self.server_tuple = server_tuple
+        self.debug = debug
+        self.quiet = quiet
         self.nick = nick
         self.channel_list = channel_list
         self.connected = False
+        self.recvloop()
 
     def connect(self):
         if not self.connected:
@@ -17,15 +31,52 @@ class IRCBot(object):
                 self.connected = True
             except Exception as e:
                 print "Bot not connected: " + str(e)
+                return
+            self.sendline("USER " + self.nick + " "  + self.nick + " " + self.nick + " :hi" )
+            self.setnick(self.nick)
+            print "Bot connected"
         else:
             print "Bot already connected"
+
+    def close(self):
+        if self.connected:
+            self.socket.close()
+            self.connected = False
+
+    def reconnect(self):
+        self.close()
+        self.connect()
+
+    @thread
+    def recvloop(self):
+        while 1:
+            if self.connected:
+                try:
+                    out = self.socket.recv(2048)
+                    if not self.quiet:
+                        print out
+                    if out.startswith("PING"):
+                        self.sendline("PONG :" + self.server_tuple[0])
+                except socket.timeout:
+                    pass
+            sleep(.5)
+
+
 
     def setnick(self, nick):
         self.sendline("NICK " + nick)
 
     def sendline(self, text):
         if self.connected:
-            self.socket.send(text + "\n")
+            try:
+                if self.debug:
+                    print("DEBUG: " + text)
+                    self.socket.send(text + "\n")
+            except socket.timeout as e:
+                self.connected = False
+                print "Connection was lost!: " + str(e)
+
+
         else:
             print "Bot is not connected"
 
@@ -38,3 +89,6 @@ class IRCBot(object):
 
     def leave_one(self, channel):
         self.sendline("PART " + channel)
+
+    def msg(self, chan_usr, msg):
+        self.sendline("PRIVMSG " + chan_usr + " :" + msg)
